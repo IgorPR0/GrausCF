1d268d79e184cd81b83b5ff48280df17089f49b0
IgorPR0 <igortecnologia2020@gmail.com>
2020-09-30 13:35:10 -0300

    Feito atividade GrausFC

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fahren;
+            double Celsius = 97;
+
+           Console.Write("Digite o valor: ");
+           Fahren = double.Parse(Console.ReadLine()); 
+           Celsius = (Fahren - 32) /1.8;
+           Console.WriteLine("Sua conversão de Fahren para Celsius é: {0}", Math.Round(Celsius,2)  );
+ 
+
+        }
+    }
+}
